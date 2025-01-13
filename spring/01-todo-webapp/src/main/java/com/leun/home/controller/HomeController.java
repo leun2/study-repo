11f44bf14216bddb03ec36f23af190861cc3ac0b
leun2 <leun12968@gmail.com>
2025-01-13 20:28:27 +0900

    25-01-13 rename controller

--- spring/01-todo-webapp/src/main/java/com/leun/home/controller/HomeController.java
+++ spring/01-todo-webapp/src/main/java/com/leun/home/controller/HomeController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class MainController {
+public class HomeController {
 
     @GetMapping
-    public String main() {
+    public String home() {
         return "index";
     }
 }
