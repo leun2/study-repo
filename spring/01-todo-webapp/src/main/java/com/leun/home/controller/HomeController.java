ce319fa5b2650233ef349a17eafc466b14afd3bd
leun2 <leun12968@gmail.com>
2025-01-14 20:24:27 +0900

    25-01-14 change todo home

--- spring/01-todo-webapp/src/main/java/com/leun/home/controller/HomeController.java
+++ spring/01-todo-webapp/src/main/java/com/leun/home/controller/HomeController.java
@@ -1,16 +1,20 @@
 package com.leun.home.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("name")
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    public String home() {
-        return "index";
+    public String getHome(ModelMap modelMap) {
+        modelMap.put("name", "lee");
+        return "home";
     }
 }
 
