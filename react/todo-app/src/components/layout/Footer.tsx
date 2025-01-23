6048e1490d7f0bb36dd16274f80da814297aed85
leun2 <leun12968@gmail.com>
2025-01-23 19:43:05 +0900

    feat: add components and Todo.css for layout styling

---
+++ react/todo-app/src/components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <div className="container">
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ react/todo-app/src/components/layout/Header.tsx
@@ -0,0 +1,53 @@
+import React, { useContext } from "react";
+import {Link} from 'react-router-dom'
+import { useAuth } from "components/auth/AuthContext";
+
+function Header() {
+
+    const authContext = useAuth()
+
+    function logout() {
+        authContext.logout()
+    }
+
+    return (
+        <div className="header">
+            <header className="border-bottom border-light border-5 mb-5 p-2">
+                <div className="container">
+                    <div className="row">
+                        <nav className="navbar navbar-expand-lg">
+                            <div className="collapse navbar-collapse">
+                                <ul className="navbar-nav">
+                                    {authContext.authState.isAuthenticated && (
+                                        <li className="nav-item fs-5">
+                                            <Link className="nav-link" to="/todos">
+                                                Home
+                                            </Link>
+                                        </li>
+                                    )}
+                                </ul>
+                            </div>
+                            <ul className="navbar-nav">
+                                {!authContext.authState.isAuthenticated ? (
+                                    <li className="nav-item fs-5">
+                                        <Link className="nav-link" to="/">
+                                            Login
+                                        </Link>
+                                    </li>
+                                ) : (
+                                    <li className="nav-item fs-5">
+                                        <Link className="nav-link" to="/" onClick={logout}>
+                                            Logout
+                                        </Link>
+                                    </li>
+                                )}
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
