4a443b8610cf7fc1301e7f2e1903bf5f2150354f
leun2 <leun12968@gmail.com>
2025-01-23 19:42:17 +0900

    fix: update for component and style changes

--- react/todo-app/src/App.js
+++ react/todo-app/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+// import './App.css';
+import React from 'react';
+import Todo from 'components/todo/Todo'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <Todo />
       </header>
     </div>
   );
