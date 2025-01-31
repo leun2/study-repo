0cf1a15b6890794d4094c1145608dfe364ef40a5
leun2 <leun12968@gmail.com>
2025-02-01 02:55:19 +0900

    feat: add API integrate for todos

--- react/todo-app/src/components/todo/Todos.tsx
+++ react/todo-app/src/components/todo/Todos.tsx
@@ -1,38 +1,93 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { retrieveAllTodosForUserNameApi, deleteTodoApi } from "api/TodoApi";
+import { useAuth } from "components/auth/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+interface Todo {
+    todoId : number;
+    todoTitle : string;
+    todoDescription: string;
+    todoDone : boolean;
+    todoDate : Date;
+}
 
 function Todos() {
-    const today = new Date()
 
-    const targetDate = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    const authContext = useAuth()
 
-    // const todos = [
-    //     { id: 1, description : "Learn AWS", done: false, date: targetDate },
-    //     { id: 2, description: "Practice React", done: false, date: targetDate },
-    // ]
+    const username = authContext.authState.username
 
-    const [todos, setTodos] = useState([
-        { id: 1, description: "Learn AWS", done: false, date: targetDate },
-        { id: 2, description: "Practice React", done: false, date: targetDate },
-    ]);
+    const navigate = useNavigate()
+    
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const [message, setMessage] = useState<String | null>(null);
+
+    useEffect(() => 
+        getAllTodos()
+    , []);
+
+    function getAllTodos(){
+        retrieveAllTodosForUserNameApi(username)
+            .then(response => {
+                setTodos(response.data)
+            })
+            .catch((error) => errorResponse(error))
+    }
+
+    function deleteTodo(todoId : number) {
+        deleteTodoApi(username, todoId)
+        .then(
+            () => {
+                setMessage(`delete todo ${todoId}`)
+                getAllTodos()
+            }
+        )
+        .catch((error) => errorResponse(error))
+    }
+
+    function updateTodo(todoId : number) {
+        navigate(`/todo/${todoId}`)
+    }
+
+    function errorResponse(error:any) {
+        console.log(error)
+    }
 
     return (
         <div className="container">
+            {message && <div className="alert alert-warning">{message}</div>}
             <table className="table">
                 <thead>
-                    <tr>
-                        <th>id</th>
+                    <tr><th>id</th>
+                        <th>title</th>
                         <th>description</th>
                         <th>done</th>
                         <th>date</th>
+                        <th>delete</th>
+                        <th>update</th>
                     </tr>
                 </thead>
                 <tbody>
                     {todos.map((todo, index) => (
                         <tr key={index}>
-                            <td>{todo.id}</td>
-                            <td>{todo.description}</td>
-                            <td>{todo.done.toString()}</td>
-                            <td>{todo.date.toDateString()}</td>
+                            <td>{todo.todoId}</td>
+                            <td>{todo.todoTitle}</td>
+                            <td>{todo.todoDescription}</td>
+                            <td>{todo.todoDone ? 'true' : 'false'}</td>
+                            <td>{todo.todoDate.toString()}</td>
+                            <td>
+                                <button className="btn btn-warning" 
+                                        onClick={() => deleteTodo(todo.todoId)}>
+                                            delete
+                                </button>
+                            </td>
+                            <td>
+                                <button className="btn btn-success"
+                                        onClick={() => updateTodo(todo.todoId)}>
+                                            update
+                                </button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
