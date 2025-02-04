ef4bdc7f5e49dfb5015ae9ccfa9b6d14f826c365
leun2 <leun12968@gmail.com>
2025-02-04 17:54:32 +0900

    init: initialize unit testing application

---
+++ spring/unit-testing/src/test/java/com/leun/UnitTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.leun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class UnitTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
