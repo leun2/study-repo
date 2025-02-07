ddea90b4f32384ef7b18f6f0a39c623e078ec04f
leun2 <leun12968@gmail.com>
2025-02-08 02:03:37 +0900

    refactor: rename methods for clarity

--- spring/todo-webapp/src/test/java/com/leun/todo/TodoControllerTest.java
+++ spring/todo-webapp/src/test/java/com/leun/todo/TodoControllerTest.java
@@ -76,7 +76,7 @@ class TodoControllerTest {
 
     @Test
     @WithMockUser(username = "lee", roles = {"USER", "ADMIN"})
-    void postTodo_ShouldRedirectToTodo_WhenValidDataIsProvided() throws Exception {
+    void postTodo_ShouldRedirectToTodo_WhenValidDataProvided() throws Exception {
 
         String description = "TodoController Test";
 
@@ -100,7 +100,7 @@ class TodoControllerTest {
 
     @Test
     @WithMockUser(username = "lee", roles = {"USER", "ADMIN"})
-    void postTodo_ShouldReturnAddTodo_WhenInvalidData() throws Exception {
+    void postTodo_ShouldReturnAddTodo_WhenInvalidDataProvided() throws Exception {
         mockMvc.perform(post("/add-todo")
                 .param("todoDescription", "short")
                 .param("todoDate", "invalid-date")
@@ -138,7 +138,7 @@ class TodoControllerTest {
 
     @Test
     @WithMockUser(username = "lee", roles = {"USER", "ADMIN"})
-    void updateTodo_ShouldRedirectToTodo_WhenValidDataIsProvided() throws Exception {
+    void updateTodo_ShouldRedirectToTodo_WhenValidDataProvided() throws Exception {
 
         when(todoService.getUserName()).thenReturn(username);
 
