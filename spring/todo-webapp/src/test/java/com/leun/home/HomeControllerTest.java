213ded5d49d69ebe4f5399a3e4f3031d5b50a00e
leun2 <leun12968@gmail.com>
2025-02-07 16:26:44 +0900

    test: add unit test for HomeController

---
+++ spring/todo-webapp/src/test/java/com/leun/home/HomeControllerTest.java
@@ -0,0 +1,49 @@
+package com.leun.home;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import com.leun.home.controller.HomeController;
+import com.leun.home.service.HomeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+@ExtendWith(MockitoExtension.class)
+@WebMvcTest(controllers = HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private HomeService homeService;
+
+//    @InjectMocks
+//    private HomeController homeController;
+
+    @BeforeEach
+    void setUp() {
+//        mockMvc = MockMvcBuilders.standaloneSetup(homeController)
+//            .build();
+    }
+
+    @Test
+    @WithMockUser(username = "lee", roles = {"USER", "ADMIN"})
+    void getHome_ShouldReturnHomeViewWithModelAttribute() throws Exception {
+        when(homeService.getUserName()).thenReturn("lee");
+
+        mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("home"))
+            .andExpect(model().attribute("name", "lee"));
+    }
+}
