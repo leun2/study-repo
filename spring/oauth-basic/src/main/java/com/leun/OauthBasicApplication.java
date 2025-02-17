3655452c95ad43b0d2b1bdfd9d8b2eef6c5172ea
leun2 <leun12968@gmail.com>
2025-02-17 20:08:59 +0900

    init: initial commit for oauth-basic

---
+++ spring/oauth-basic/src/main/java/com/leun/OauthBasicApplication.java
@@ -0,0 +1,13 @@
+package com.leun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthBasicApplication.class, args);
+	}
+
+}
---
+++ spring/oauth-basic/src/test/java/com/leun/OauthBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
