534316346556ad29a1beeef30fc3d07d0c07e7ae
leun2 <leun12968@gmail.com>
2025-02-18 23:46:41 +0900

    init: initial commit for aop-basic

---
+++ spring/aop-basic/src/main/java/com/leun/AopBasicApplication.java
@@ -0,0 +1,13 @@
+package com.leun.aop_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AopBasicApplication.class, args);
+	}
+
+}
