5731c4a31dc428cf9be2ca637dee183f1d6be4b4
leun2 <leun12968@gmail.com>
2025-02-19 23:13:33 +0900

    feat: add CommandLineRunner to verify AOP logging

--- spring/aop-basic/src/main/java/com/leun/AopBasicApplication.java
+++ spring/aop-basic/src/main/java/com/leun/AopBasicApplication.java
@@ -1,13 +1,29 @@
-package com.leun.aop_basic;
+package com.leun;
 
+import com.leun.test.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AopBasicApplication {
+public class AopBasicApplication implements CommandLineRunner {
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	private final TestService testService;
+
+	public AopBasicApplication(TestService testService) {
+		this.testService = testService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AopBasicApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("Value returned is {}", testService.calculateMax());
+	}
 }
