8f6372dd91444701a3a92d0a452fb0f089f1b690
leun2 <leun12968@gmail.com>
2025-06-05 00:09:35 +0900

    feat: Add Fibonacci class with method to find all implemented interfaces

---
+++ java/reflection/src/class_analysis/Fibonacci.java
@@ -0,0 +1,19 @@
+package class_analysis;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Fibonacci {
+
+    public static Set<Class<?>> findAllImplementedInterfaces(Class<?> input) {
+        Set<Class<?>> allImplementedInterfaces = new HashSet<>();
+
+        Class<?>[] inputInterfaces = input.getInterfaces();
+        for (Class<?> currentInterface : inputInterfaces) {
+            allImplementedInterfaces.add(currentInterface);
+            allImplementedInterfaces.addAll(findAllImplementedInterfaces(currentInterface));
+        }
+
+        return allImplementedInterfaces;
+    }
+}
